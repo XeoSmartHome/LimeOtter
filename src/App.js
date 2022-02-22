39cec3abc3c11390bc4af821c6f328fd96b95960
Claudiu Neamtu <xeosmarthome@gmail.com>
2022-02-22 17:02:48 +0200

    Add Claudiu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => {
                 <p>
                     Welcome to <b style={{color: "orange"}}>XeoSmartHome</b>!
                 </p>
+                <p>
+                    Claudiu
+                </p>
             </header>
         </div>
     );
